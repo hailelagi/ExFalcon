4ec74ac71f25372795d35d229082c930302768ec
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-05-15 19:41:53 +0100

    feat: client calls

--- lib/ex_falcon.ex
+++ lib/ex_falcon.ex
@@ -1,17 +1,65 @@
 defmodule ExFalcon do
   @moduledoc """
-  todo: Documentation for `ExFalcon`.
+    ExFalcon is a library that wraps the FalconX API.
   """
   @behaviour ExFalcon.Behaviour
 
+  alias ExFalcon.Behaviour, as: T
   alias ExFalcon.Client
 
+  @doc """
+  Gets a list of trading pairs you are eligible to trade.
+
+  Example:
+  ExFalcon.get_trading_pairs()
+  [{'base_token': 'BTC', 'quote_token': 'USD'}, {'base_token': 'ETH', 'quote_token': 'USD'}]
+  """
+
+  @spec get_trading_pairs() :: [T.token_pair()] | T.error()
   def get_trading_pairs do
-    Client.get_pairs()
+    case Client.pairs() do
+      {:ok, pairs} -> pairs
+      error -> error
+    end
   end
 
+  @doc """
+  Gets a two_way, buy or sell quote for a token pair.
+
+  Example:
+    ExFalcon.get_quote("BTC", "USD", 10, :buy)
+
+    {
+      "status": "success",
+      "fx_quote_id": "00c884b056f949338788dfb59e495377",
+      "buy_price": 12650,
+      "sell_price": null,
+      "token_pair": {
+        "base_token": "BTC",
+        "quote_token": "USD"
+      },
+      "quantity_requested": {
+        "token": "BTC",
+        "value": "10"
+      },
+      "side_requested": "buy",
+      "t_quote": "2019-06-27T11:59:21.875725+00:00",
+      "t_expiry": "2019-06-27T11:59:22.875725+00:00",
+      "is_filled": false,
+      "side_executed": null,
+      "price_executed": null,
+      "t_execute": null,
+      "client_order_id": "d6f3e1fa-e148-4009-9c07-a87f9ae78d1a"
+    }
+  """
+  @spec get_quote(String.t(), String.t(), float(), T.side()) :: T.quote_response() | T.error()
   def get_quote(base, fx_quote, quantity, side) do
-    nil
+    opts = [base: base, quote: fx_quote, quantity: quantity, side: side]
+
+    case Client.quotes(opts) do
+      {:ok, fx_quote} -> fx_quote
+      error -> error
+    end
   end
 
   def place_order(
--- lib/ex_falcon/client/client.ex
+++ lib/ex_falcon/client/client.ex
@@ -16,10 +16,67 @@ defmodule ExFalcon.Client do
 
   plug(Tesla.Middleware.JSON)
 
-  def get_pairs do
-    case Tesla.get("/pairs") do
-      {:ok, tesla} -> tesla.body
-      {:error, error} -> error
+  def pairs, do: Tesla.get("/pairs") |> parse_response()
+
+  def quotes(opts), do: Tesla.get("/quotes", body: opts) |> parse_response()
+
+  def execute_quote(opts), do: Tesla.post("/quotes/execute", body: opts) |> parse_response()
+
+  def executed_quotes, do: Tesla.get("/quotes")
+
+  def place_order(opts), do: Tesla.post("/order", body: opts)
+
+  def order_history, do: Tesla.get("/orders") |> parse_response()
+
+  def balances, do: Tesla.get("/balances") |> parse_response()
+
+  def total_balances, do: Tesla.get("/balances/total")
+
+  def transfer(id), do: Tesla.get("/transfer/#{id}") |> parse_response()
+
+  def transfers, do: Tesla.get("/transfers") |> parse_response()
+
+  def thirty_day_trailing_volume, do: Tesla.get("/get_30_day_trailing_volume") |> parse_response()
+
+  def trade_volume, do: Tesla.get("/get_trade_volume") |> parse_response()
+
+  def token_balance_info(token), do: Tesla.get("/get_trade_volume#{token}") |> parse_response()
+
+  def request_withdrawal(ref_id \\ nil, opts) do
+    path = if ref_id == nil, do: "/submit_withdrawal_request", else: "withdraw"
+    Tesla.post(path, opts) |> parse_response()
+  end
+
+  def rate_limit, do: Tesla.get("rate_limit/") |> parse_response()
+
+  def trade_limits(platform), do: Tesla.get("/get_trade_limits/#{platform}") |> parse_response()
+
+  def trade_sizes(), do: Tesla.get("/get_trade_sizes") |> parse_response()
+
+  defp parse_response(request) do
+    case request do
+      {:ok, %{status: 200, body: %{status: "success"} = body}} -> {:ok, body}
+      response -> parse_error(response)
+    end
+  end
+
+  def parse_error(response) do
+    # todo: handle Falcon errors
+    case response do
+      {:ok, %{status: 400, body: _body}} ->
+        {:error, %{code: 400, reason: "Bad Request"}}
+
+      {:ok, %{status: 401, body: _body}} ->
+        {:error, %{code: 401, reason: "Invalid API Key"}}
+
+      {:ok, %{status: 403, body: _body}} ->
+        {:error, %{code: 403, reason: "Forbidden"}}
+
+      {:ok, %{status: 404, body: _body}} ->
+        {:error, %{code: 404, reason: "Resource Not Found"}}
+
+      {:ok, %{status: 500, body: _body}} ->
+        {:error, %{code: 500, reason: "Internal Server Error"}}
     end
   end
 end
